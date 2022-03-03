ec7307a9ff215a89fd1d82d75e7ff832a9de7633
harsh gandhi <harshgandhi043@gmail.com>
2022-03-03 22:44:00 +0530

    made basic layout for playing song

--- app/src/main/java/com/example/serviceapp/MainActivity.java
+++ app/src/main/java/com/example/serviceapp/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.serviceapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnPlay,btnPause,btnStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPlay = findViewById(R.id.btnPlay);
+        btnPause = findViewById(R.id.btnPause);
+        btnStop = findViewById(R.id.btnStop);
+
+        btnPlay.setOnClickListener(view -> playSong());
+        btnPause.setOnClickListener(view -> pauseSong());
+        btnStop.setOnClickListener(view -> stopSong());
+    }
+
+    private void playSong(){
+
+    }
+
+    private void pauseSong(){
+
+    }
+    private void stopSong(){
+
     }
 }
